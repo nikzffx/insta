7896c02067e27bdb0b255c8e19124164248da0bc
nikzffx <nikzbahi@gmail.com>
2025-02-09 23:38:23 +0530

    Update nikz.py

--- nikz.py
+++ nikz.py
@@ -1,7 +1,13 @@
 import requests
 from cfonts import render
 
-print("")
+NIKZ = render('{NiKZ}', colors=['yellow', 'cyan'], align='center')
+print(NIKZ)
+print("▩" * 60)
+
+print(f"""\x1b[38;5;117m 1\x1b[38;5;231m - Gmail Specific Year + Meta Hunter | ✅
+\x1b[38;5;117m 2\x1b[38;5;231m - Pass reset Tool | \x1b[1;32m Active ✅
+""")
 
 def main_menu():
     print("▩" * 60)
