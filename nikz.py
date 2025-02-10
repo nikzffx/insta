11eb941e4c24b28048084253e34aed644a82efc6
nikzffx <nikzbahi@gmail.com>
2025-02-10 09:25:05 +0530

    Update nikz.py

--- nikz.py
+++ nikz.py
@@ -7,16 +7,17 @@ print(NIKZ)
 print("▩" * 60)
 
 print(f"""\x1b[38;5;117m 1\x1b[38;5;231m - Gmail Specific Year + Meta Hunter | \x1b[1;32m Active ✅
-\x1b[38;5;117m 2\x1b[38;5;231m - 5L Username Tool | \x1b[1;32m Active ✅
-\x1b[38;5;117m 3\x1b[38;5;231m - Pass Reset Tool | \x1b[1;32m Active ✅
+\x1b[38;5;117m 2\x1b[38;5;231m - Auto Reporting Tool | \x1b[1;32m Active ✅
+\x1b[38;5;117m 3\x1b[38;5;231m - 5L Username Tool | \x1b[1;32m Active ✅
+\x1b[38;5;117m 4\x1b[38;5;231m - Pass Reset Tool | \x1b[1;32m Active ✅
 """)
 
 # Dictionary of script URLs
 scripts = {
     "1": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/meta.py",
-    "2": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/5lusername.py",
-    "3": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/passrest.py",
-    "4": "",
+    "2": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/reportingtool.py",
+    "3": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/5lusername.py",
+    "4": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/passrest.py",
     "5": "",
     "6": "",
 }
