75a1752867171312116279ed7dd556d13345f9d5
nikzffx <nikzbahi@gmail.com>
2025-02-10 05:53:34 +0530

    Update nikz.py

--- nikz.py
+++ nikz.py
@@ -7,14 +7,15 @@ print(NIKZ)
 print("▩" * 60)
 
 print(f"""\x1b[38;5;117m 1\x1b[38;5;231m - Gmail Specific Year + Meta Hunter | \x1b[1;32m Active ✅
-\x1b[38;5;117m 2\x1b[38;5;231m - Pass reset Tool | \x1b[1;32m Active ✅
+\x1b[38;5;117m 2\x1b[38;5;231m - 5L Username Tool | \x1b[1;32m Active ✅
+\x1b[38;5;117m 3\x1b[38;5;231m - Pass Reset Tool | \x1b[1;32m Active ✅
 """)
 
 # Dictionary of script URLs
 scripts = {
     "1": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/meta.py",
-    "2": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/passrest.py",
-    "3": "",
+    "2": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/5lusername.py",
+    "3": "https://raw.githubusercontent.com/nikzffx/insta/refs/heads/main/passrest.py",
     "4": "",
     "5": "",
     "6": "",
@@ -32,7 +33,7 @@ def execute_script(url):
 def main_menu():
     """Display menu and handle user input."""
     print("▩" * 60)
-    choice = input(" • Enter your choice (1-2): ")
+    choice = input(" • Enter your choice (1-3): ")
 
     if choice in scripts:
         execute_script(scripts[choice])
